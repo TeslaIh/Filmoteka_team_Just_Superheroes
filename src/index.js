1050095f490ef9c92033132c51a2df0afd757e54
in-love-with-anime <vall.bell91@gmail.com>
2022-01-19 18:23:48 +0200

    Forgot about library

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './sass/main.scss';
 import './js/films-gallery';
 import './js/my-library';
-import './js/modal';
 import './js/header-bg';
-import './js/themeSwitcher'
-import './js/arrowBtn.js'
+import './js/modal';
+import './js/themeSwitcher';
+import './js/arrowBtn.js';
