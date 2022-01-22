951115e30056a5245800b46e6ee1499444b4ea71
in-love-with-anime <vall.bell91@gmail.com>
2022-01-22 20:25:58 +0200

    a lot of changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './js/search-films';
 import './js/films-gallery';
 import './js/my-library';
 import './js/header-bg';
-import './js/modal';
+import './js/film-modal';
 import './js/themeSwitcher';
 import './js/arrowBtn';
 import './js/team-cards';
