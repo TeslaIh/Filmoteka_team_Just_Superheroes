2146162ce36b4d0197617c6b16354b3f2b5c778c
romanchenko2804 <romanchenko2804@gmail.com>
2022-01-25 21:33:47 +0200

    add films to libtaty

--- src/js/my-library.js
+++ src/js/my-library.js
@@ -1,12 +1,9 @@
-import Notiflix from 'notiflix';
-
 const refs = {
   libraryBtn: document.querySelector('.nav__library'),
   gallery: document.querySelector('.card-set'),
   watchedEl: document.querySelector('.button__watched'),
   queueEl: document.querySelector('.button__queue'),
   sectionEl: document.querySelector('.gallery'),
-  headerEl: document.querySelector('.header'),
   paginationEl: document.querySelector('.pagination-section'),
 };
 
@@ -14,85 +11,72 @@ refs.libraryBtn.addEventListener('click', onLibraryBtnClick);
 refs.watchedEl.addEventListener('click', onWatchedBtnClick);
 refs.queueEl.addEventListener('click', onQueueBtnClick);
 
-window.addEventListener('storage', locStor); //слушатель на сторедж
-
-
-const WATCHED_KEY = 'LocWatched';
-const QUEUE_KEY = 'LocQueue';
-
-const arrayWatched = JSON.parse(localStorage.getItem(WATCHED_KEY));
-const arrayQueue = JSON.parse(localStorage.getItem(QUEUE_KEY));
-
-console.log('arrayWatched: ', arrayWatched);
-console.log('LocQueue: ', arrayQueue);
-
-function renderGalleryFilms(array) {
-  if (array) {
-    const markup = array
-      .map(el => {
-        if (el.genre_ids.length > 3) {
-          el.genre_ids.splice(2, el.genre_ids.length);
-          el.genre_ids.push('Other');
-        }
-        return `<li class='card-set_item'>
-        <img class="card-set_img" src="https://image.tmdb.org/t/p/w500${el.poster_path}" alt='film'>
-        <div class="card-set_box">
-        <p class="card-set_text">${el.title}</p>
-        <p class="card-set_genres">${el.genre_ids.join(', ')} &#124 ${el.release_date.slice(
-          0,
-          4,
-        )}</p>
-        </div>
-        </li>`;
-      })
-      .join('');
-    refs.gallery.insertAdjacentHTML('beforeend', markup);
+window.addEventListener('storage', e => {
+  if (e.key === 'LocWatched' || e.key === 'LocQueue') {
+    window.location.reload();
   }
-}
+});
 
-function locStor(e) {  // функция, с помощью которой проверил, что если открыть две вкладки, то консол.лог отработает в другой вкладке (не в той где производятся действия с ЛокалСторедж) 
-  console.log(e);
-}
-
-function onLibraryBtnClick() {
+function onLibraryBtnClick(e) {
   refs.gallery.innerHTML = '';
   refs.paginationEl.innerHTML = '';
+  const arrayWatched = JSON.parse(localStorage.getItem('LocWatched'));
 
-  if (localStorage.getItem(WATCHED_KEY) !== null) {
+  if (localStorage.getItem('LocWatched') !== null) {
     return renderGalleryFilms(arrayWatched);
   }
 
   refs.sectionEl.classList.add('watched--bg');
 }
 
+function renderGalleryFilms(array) {
+  const markup = array
+    .map(el => {
+      let date = '21 Century';
+      let img = 'https://kinomaiak.ru/wp-content/uploads/2018/02/noposter.png';
+      if (el.genre_ids.length > 3) {
+        el.genre_ids.splice(2, el.genre_ids.length);
+        el.genre_ids.push('Other');
+      }
+
+      if (el.hasOwnProperty('release_date') && el.release_date.length === 10) {
+        date = el.release_date.slice(0, 4);
+      }
+      if (el.poster_path !== null) {
+        img = `https://image.tmdb.org/t/p/w500${el.poster_path}`;
+      }
+
+      return `<li class='card-set_item'>
+        <img class="card-set_img" src="${img}" alt='film'>
+        <div class="card-set_box">
+        <p class="card-set_text">${el.title}</p>
+        <p class="card-set_genres">${el.genre_ids.join(', ')} &#124 ${date}</p>
+        <p class="card-set_vote">${el.vote_average}</p>
+        </div>
+        </li>`;
+    })
+    .join('');
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
 
 function onWatchedBtnClick() {
   refs.gallery.innerHTML = '';
+  const arrayWatched = JSON.parse(localStorage.getItem('LocWatched'));
 
-  if (localStorage.getItem(WATCHED_KEY) !== null) {
+  if (localStorage.getItem('LocWatched') !== null) {
     refs.sectionEl.classList.remove('watched--bg');
     return renderGalleryFilms(arrayWatched);
   }
-   refs.sectionEl.classList.add('watched--bg');
+  refs.sectionEl.classList.add('watched--bg');
 }
 
 function onQueueBtnClick() {
   refs.gallery.innerHTML = '';
+  const arrayQueue = JSON.parse(localStorage.getItem('LocQueue'));
 
-  if (localStorage.getItem(QUEUE_KEY) !== null) {
+  if (localStorage.getItem('LocQueue') !== null) {
     refs.sectionEl.classList.remove('watched--bg');
     return renderGalleryFilms(arrayQueue);
   }
-   refs.sectionEl.classList.add('watched--bg');
+  refs.sectionEl.classList.add('watched--bg');
 }
-
-function reloadPage() {
-  window.location.reload();
-}
-
-// window.dispatchEvent(new Event('storage'));   // - то, что скинул саша в чат
- 
-// window.addEventListener('storage', e => {
-// }
-// );
-
