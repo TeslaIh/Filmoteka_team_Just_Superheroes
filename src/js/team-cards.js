975f24a954a7fd99f9f5bc9c05450f9db856719c
TeslaIh <87144117+TeslaIh@users.noreply.github.com>
2022-01-26 20:08:38 +0200

    additional photos

--- src/js/team-cards.js
+++ src/js/team-cards.js
@@ -1,7 +1,16 @@
-window.addEventListener('keydown', closeModalByEscape);
-function closeModalByEscape(e) {
-    if (e.code === 'Escape') {
-        modal.close();
-        window.removeEventListener('keydown', closeModalByEscape);
-    }
-}
+// window.addEventListener('keydown', closeModalByEscape);
+// function closeModalByEscape(evt) {
+//     if (evt.code === 'Escape') {
+//         close();
+//         window.removeEventListener('keydown', closeModalByEscape);
+//     }
+// }
+//   window.addEventListener('keydown', closeModalByEsc);
+
+// function closeModalByEsc(e) {
+//     if (e.code === 'Escape') {
+//         modalContent.close();
+
+//         window.removeEventListener('keydown', closeModalByEsc);
+//     }
+// }
